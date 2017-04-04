99bae809dbd1f40306b41cde41bda5212a96ada3
pritam-das <pritam.das@mail.utoronto.ca>
2017-04-04 15:30:22 -0400

    CRM Web: HTTP Requests and Responses assignment complete

--- crm.rb
+++ crm.rb
@@ -17,3 +17,8 @@ end
 get '/contacts/new' do
   erb :new_contact
 end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
+end
