b8e0bd6d904866a08bfff125e5d83d360fd5be19
pritam-das <pritam.das@mail.utoronto.ca>
2017-04-04 15:08:16 -0400

    regular commit

--- crm.rb
+++ crm.rb
@@ -1,5 +1,19 @@
+require_relative 'contact'
 require 'sinatra'
 
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+
 get '/' do
-  'Main Menu'
+  @crm_app_name = "Pritam's CRM"
+  erb :index
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+get '/contacts/new' do
+  erb :new_contact
 end
